80effcad99fdecfa0908676c5bf598e8f2b2a98a
smrutigangane <141392718+smrutigangane@users.noreply.github.com>
2023-12-14 14:22:08 +0530

    Add files via upload

---
+++ bim.js
@@ -0,0 +1,17 @@
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
+  
+    if (isNaN(height) || height <= 0) {
+      results.innerHTML = 'Please provide a valid height.';
+    } else if (isNaN(weight) || weight <= 0) {
+      results.innerHTML = 'Please provide a valid weight.';
+    } else {
+      const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+      results.innerHTML = `Your BMI is: <span>${bmi}</span>`;
+    }
+  });
+  
